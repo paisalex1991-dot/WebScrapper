01c6612358910eb813213fd34942872bb5e2a2e2
DESKTOP-FICODHL\Alex-PC <pais.alex1991@gmai.com>
2025-10-03 21:49:44 +0300

    Experiment file added.

---
+++ google-python-exercises/experiment.py
@@ -0,0 +1,27 @@
+
+
+## text added for git testing
+
+
+# Global variable for the results box
+result_box = None
+
+# Paths to your ChromeDriver and optional Chrome binary
+CHROME_PATH = r"C:\01_PROGRAME\Chrome\chrome-win64\chrome-win64\chrome.exe"
+CHROMEDRIVER_PATH  = r"C:\01_PROGRAME\Chrome\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+
+# Base URL
+BASE_URL = "https://mcscertified.com/product-directory/"
+
+def scrape_products_selenium():
+
+    ### new_feature1
+
+
+
+def main():
+
+##tbd
+
+if __name__ == "__main__":
+    main()
